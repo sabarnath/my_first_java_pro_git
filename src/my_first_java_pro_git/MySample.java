63ecfbbf37867f95e0a19333d192b3af1eeeab69
sabarinathan <sabarnath@gmail.com>
2015-04-09 12:35:43 +0530

    my first commit

---
+++ src/my_first_java_pro_git/MySample.java
@@ -0,0 +1,10 @@
+package my_first_java_pro_git;
+
+public class MySample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello would : ======================>,sir/madam");
+	}
+
+}
