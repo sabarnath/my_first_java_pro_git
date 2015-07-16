aee67ec0a69bdefe75ca3979bd67c46c98ba9a69
Sabarinathan S <sabarnath@gmail.com>
2015-07-16 19:59:18 +0530

    NameComparator using name in aliphatic order

---
+++ src/my_first_java_pro_git/NameComparator.java
@@ -0,0 +1,16 @@
+package com.javacodegeeks.java.util.comparator;
+
+import java.util.Comparator;
+
+public class NameComparator implements Comparator {
+
+	@Override
+	public int compare(Student o1, Student o2) {
+		String name1 = o1.getName();
+		String name2 = o2.getName();
+
+		// ascending order (descending order would be: name2.compareTo(name1))
+		return name1.compareTo(name2);
+	}
+
+}
